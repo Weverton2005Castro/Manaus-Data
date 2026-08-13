a17add8721ec5e1686a017d85ebd2325b65bb6ed
weverton2005castro <wblanco846@gmail.com>
2026-08-12 21:15:09 -0400

    desfazendo ajuste que quebrou

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import {
   MapContainer,
   TileLayer,
@@ -5,24 +6,9 @@ import {
   Popup,
 } from 'react-leaflet'
 
-import L from 'leaflet'
-
-import markerIcon from 'leaflet/dist/images/marker-icon.png'
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
-import markerShadow from 'leaflet/dist/images/marker-shadow.png'
-
-const defaultIcon = L.icon({
-  iconUrl: markerIcon,
-  iconRetinaUrl: markerIcon2x,
-  shadowUrl: markerShadow,
-
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-})
-
-L.Marker.prototype.options.icon = defaultIcon
+import { supabase } from '../lib/supabase'
+import MapClickHandler from './MapClickHandler'
+import IncidentForm from './IncidentForm'
 
 function Map() {
   const manaus = [-3.1019, -60.025]
