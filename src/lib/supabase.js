232f1c31326f88b859600f0e29f76737539d6714
Weverton2005Castro <wblanco846@gmail.com>
2026-08-25 17:11:47 -0400

    removendo console log esquecidos pelo codigo

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js"
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
-console.log('URL:', supabaseUrl)
-console.log('KEY:', supabaseAnonKey ? 'OK' : 'NÃO ENCONTRADA')
+// console.log('URL:', supabaseUrl)
+// console.log('KEY:', supabaseAnonKey ? 'OK' : 'NÃO ENCONTRADA')
 
 export const supabase = createClient(
     supabaseUrl,
